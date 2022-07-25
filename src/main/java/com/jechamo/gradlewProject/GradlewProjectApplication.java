21ac32bca31b9f64ea6ad9af5474eca5d2ce28cb
jechamo <109816631+jechamo@users.noreply.github.com>
2022-07-25 19:45:17 +0200

    Add files via upload

---
+++ src/main/java/com/jechamo/gradlewProject/GradlewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jechamo.gradlewProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlewProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jechamo/gradlewProject/GradlewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jechamo.gradlewProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
